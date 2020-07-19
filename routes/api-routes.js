450c8617fbe2254a4f3844ef8902e2ecba4fadf4
orangeblack13 <AndrewJKleiner@gmail.com>
2020-07-18 21:45:15 -0400

    routes changed to correspond with handlebar names

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,20 +6,20 @@ module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/signin", passport.authenticate("local"), function(req, res) {
     res.json(req.user);
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
+  app.post("/api/createAccount", function(req, res) {
     db.User.create({
       email: req.body.email,
       password: req.body.password
     })
       .then(function() {
-        res.redirect(307, "/api/login");
+        res.redirect(307, "/api/signin");
       })
       .catch(function(err) {
         res.status(401).json(err);
