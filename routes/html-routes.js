0241e01873df99655d694a9c9e62b80ded4b76d4
Esther Min <estherjmin@gmail.com>
2020-07-19 08:29:15 -0400

    added express handlebars package and setup in server.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,31 +1,37 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
+var express = require("express");
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+// Routes for handlebars
+module.exports = function (app) {
 
-  app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/homepage");
-    }
-    res.sendFile(path.join(__dirname, "../public/views/index.handlebars"));
+  app.get("/homepage", function (req, res) {
+    res.render("index", {});
   });
 
-  app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/homepage");
-    }
-    res.sendFile(path.join(__dirname, "../public/views/signin.handlebars"));
-  });
+  // app.get("/", function (req, res) {
+  //   // If the user already has an account send them to the members page
+  //   if (req.user) {
+  //     res.redirect("/homepage");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/views/index.handlebars"));
+  // });
+
+  // app.get("/login", function (req, res) {
+  //   // If the user already has an account send them to the members page
+  //   if (req.user) {
+  //     res.redirect("/homepage");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/views/signin.handlebars"));
+  // });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/homepage", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/index.handlebars"));
-  });
+  // app.get("/homepage", isAuthenticated, function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/views/index.handlebars"));
+  // });
 
 };
