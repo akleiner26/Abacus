f5c4281d987f2787c6bb788b345f03f979032178
Esther Min <estherjmin@gmail.com>
2020-07-20 14:39:55 -0400

    updated seeds and changed User model references to Teacher

--- config/passport.js
+++ config/passport.js
@@ -11,25 +11,25 @@ passport.use(new LocalStrategy(
   },
   function(email, password, done) {
     // When a user tries to sign in this code runs
-    db.User.findOne({
+    db.Teacher.findOne({
       where: {
         email: email
       }
-    }).then(function(dbUser) {
+    }).then(function(dbTeacher) {
       // If there's no user with the given email
-      if (!dbUser) {
+      if (!dbTeacher) {
         return done(null, false, {
           message: "Incorrect email."
         });
       }
       // If there is a user with the given email, but the password the user gives us is incorrect
-      else if (!dbUser.validPassword(password)) {
+      else if (!dbTeacher.validPassword(password)) {
         return done(null, false, {
           message: "Incorrect password."
         });
       }
       // If none of the above, return the user
-      return done(null, dbUser);
+      return done(null, dbTeacher);
     });
   }
 ));
