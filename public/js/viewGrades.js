67ebab6e5aa1c2a9cae48178644dea0d0a81c5df
Esther Min <estherjmin@gmail.com>
2020-07-21 12:48:19 -0400

    started working on view grades buttons

---
+++ public/js/viewGrades.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    let viewGradesBtns = $(".viewGradesBtns");
+
+
+    // Shows modal when "+ Add Student" button is clicked
+    viewGradesBtns.on("click", function () {
+        console.log($(this))
+    })
+});
