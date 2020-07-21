c6c4fbb385280f3cb17918f86cdfd74e2b4d2880
Esther Min <estherjmin@gmail.com>
2020-07-21 18:44:51 -0400

    fixed bug with duplicate grades displaying on students page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,148 +8,149 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 // Routes for handlebars
 module.exports = function (app) {
 
-  // Sign in page
-  app.get("/", function (req, res) {
-    res.render("signin")
-  });
-  app.get("/signin", function (req, res) {
-    res.render("signin");
-  });
-  //Create Account
-  app.get("/create-account", function (req, res) {
-    res.render("createAccount");
-  });
+	// Sign in page
+	app.get("/", function (req, res) {
+		res.render("signin")
+	});
+	app.get("/signin", function (req, res) {
+		res.render("signin");
+	});
+	//Create Account
+	app.get("/create-account", function (req, res) {
+		res.render("createAccount");
+	});
 
 
-  //Create Assignment
-  app.get("/createAssignment", function (req, res) {
-    res.render("createAssignment");
-  });
+	//Create Assignment
+	app.get("/createAssignment", function (req, res) {
+		res.render("createAssignment");
+	});
 
-  //Get all assigments
-  app.get("/assignments", function (req, res) {
+	//Get all assigments
+	app.get("/assignments", function (req, res) {
 
-    db.Assignment.findAll({ raw: true })
-      .then(function (assignmentData) {
+		db.Assignment.findAll({ raw: true })
+			.then(function (assignmentData) {
 
-        // console.log(assignmentData);
+				// console.log(assignmentData);
 
-        // Create JSON with data from terminal and pass through in res.render
+				// Create JSON with data from terminal and pass through in res.render
 
-        res.render("viewAssignments", { Assignments: assignmentData });
-      });
-  });
+				res.render("viewAssignments", { Assignments: assignmentData });
+			});
+	});
 
-  //Get single assignment
-  app.get("/assignments/:id", isAuthenticated, function (req, res) {
-    db.Assignment.findOne({ raw: true, where: {id: req.params.id}, include: [db.Student] 
-    }).then (function (assignmentData){
-      res.render("soloAssignment", assignmentData);
-    })
-   
-  });
+	//Get single assignment
+	app.get("/assignments/:id", isAuthenticated, function (req, res) {
+		db.Assignment.findOne({
+			raw: true, where: { id: req.params.id }, include: [db.Student]
+		}).then(function (assignmentData) {
+			res.render("soloAssignment", assignmentData);
+		})
 
-  // View by students page 
-  app.get("/students", isAuthenticated, function (req, res) {
+	});
 
-    db.Assignment.findAll({ raw: true })
-      .then(function (assignmentData) {
-        // let assignmentCount = assignmentData.length;
+	// View by students page 
+	app.get("/students", isAuthenticated, function (req, res) {
 
-
-        // req.user.id grabs the id of currently signed in user from passport
-        db.Student.findAll({
-          raw: true,
-          include: [db.Grade],
-          where: {
-            TeacherId: req.user.id
-          }
-        })
-          .then(function (userData) {
-            // console.log(userData);
-
-            let studentData = [];
-            let studentObj = {};
-            let studentId = 0;
-            let assignmentId = 0;
-
-            for (let i = 0; i <= userData.length; i++) {
-
-              // Pushes last student into student object
-              if (i === userData.length) {
-                while (assignmentId < assignmentData.length) {
-                  studentObj.grades.push(null);
-                  assignmentId++;
-                  // console.log(`New assignment ID is ${assignmentId}`)
-                }
-                studentData.push(studentObj);
-              } else {
-
-                if (studentId !== userData[i].id) {
-                  if (i > 0) {
-                    // console.log(`Current assignment ID is ${assignmentId}`)
-                    while (assignmentId < assignmentData.length) {
-                      studentObj.grades.push(null);
-                      assignmentId++;
-                      // console.log(`New assignment ID is ${assignmentId}`)
-                    }
-
-                    studentData.push(studentObj)
-                  };
-
-                  studentObj = {};
-                  assignmentId = 0;
-                  studentId = userData[i].id;
-                  studentObj.grades = [];
-
-                  studentObj.first_name = userData[i].first_name;
-                  studentObj.last_name = userData[i].last_name;
-                }
+		db.Assignment.findAll({ raw: true })
+			.then(function (assignmentData) {
+				// let assignmentCount = assignmentData.length;
 
 
-                if (userData[i]['Grades.AssignmentId'] !== null) {
-                  assignmentId = userData[i]['Grades.AssignmentId'];
+				// req.user.id grabs the id of currently signed in user from passport
+				db.Student.findAll({
+					raw: true,
+					include: [db.Grade],
+					where: {
+						TeacherId: req.user.id
+					}
+				})
+					.then(function (userData) {
+						// console.log(userData);
 
-                  console.log(`Reassigned assignment ID is ${assignmentId}`)
+						let studentData = [];
+						let studentObj = {};
+						let studentId = 0;
+						let assignmentId = 0;
 
-                  studentObj.grades.push(userData[i]['Grades.gradeVal']);
-                }
+						for (let i = 0; i <= userData.length; i++) {
 
-                      // console.log(`Reassigned assignment ID is ${assignmentId}`)
-  
-                      studentObj.grades.push(userData[i]['Grades.gradeVal']);
-                    }
-                  
-              }
+							// Pushes last student into student object
+							if (i === userData.length) {
+								while (assignmentId < assignmentData.length) {
+									studentObj.grades.push(null);
+									assignmentId++;
+									// console.log(`New assignment ID is ${assignmentId}`)
+								}
+								studentData.push(studentObj);
+							} else {
+
+								if (studentId !== userData[i].id) {
+									if (i > 0) {
+										// console.log(`Current assignment ID is ${assignmentId}`)
+										while (assignmentId < assignmentData.length) {
+											studentObj.grades.push(null);
+											assignmentId++;
+											// console.log(`New assignment ID is ${assignmentId}`)
+										}
+
+										studentData.push(studentObj)
+									};
+
+									studentObj = {};
+									assignmentId = 0;
+									studentId = userData[i].id;
+									studentObj.grades = [];
+
+									studentObj.first_name = userData[i].first_name;
+									studentObj.last_name = userData[i].last_name;
+								}
 
 
-             
-            // console.log(studentData);
+								if (userData[i]['Grades.AssignmentId'] !== null) {
+									assignmentId = userData[i]['Grades.AssignmentId'];
 
-            res.render("viewByStudents", { students: studentData, assignments: assignmentData });
-            // });
-          });
-      })
-  });
+									console.log(`Reassigned assignment ID is ${assignmentId}`)
 
-  // Create Account
-  app.get("/createAccount", function (req, res) {
+									studentObj.grades.push(userData[i]['Grades.gradeVal']);
+								}
 
-    res.render("createAccount");
-  });
-  //Teacher Portal
-  app.get("/userportal", isAuthenticated, function (req, res) {
+								// console.log(`Reassigned assignment ID is ${assignmentId}`)
 
-    res.render("index");
-  });
 
-  //Grades
-  app.get("/grades", isAuthenticated, function (req, res) {
-    res.render("grades");
-  });
+							}
 
-  //Catch all
-  app.get("*", function (req, res) {
-    res.render("index");
-  });
+						}
+
+
+
+						// console.log(studentData);
+
+						res.render("viewByStudents", { students: studentData, assignments: assignmentData });
+						// });
+					});
+			})
+	});
+
+	// Create Account
+	app.get("/createAccount", function (req, res) {
+
+		res.render("createAccount");
+	});
+	//Teacher Portal
+	app.get("/userportal", isAuthenticated, function (req, res) {
+
+		res.render("index");
+	});
+
+	//Grades
+	app.get("/grades", isAuthenticated, function (req, res) {
+		res.render("grades");
+	});
+
+	//Catch all
+	app.get("*", function (req, res) {
+		res.render("index");
+	});
 };
