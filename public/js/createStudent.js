3dbc07218b294fb9605937b23de7c5e2d856e111
orangeblack13 <AndrewJKleiner@gmail.com>
2020-07-22 23:42:19 -0400

    Debugged

--- public/js/createStudent.js
+++ public/js/createStudent.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     let saveBtn = $("#saveBtn")
     let closeBtn = $("#closeBtn")
     let firstNameInput = $("input#first-name-input");
-	let lastNameInput = $("input#last-name-input");
-	let teacherIdInput = $("input#teacher-id-input");
+    let lastNameInput = $("input#last-name-input");
+    let teacherIdInput = $("input#teacher-id-input");
     // Shows modal when "+ Add Student" button is clicked
     addBtn.on("click", function () {
         modal.css("display", "block");
@@ -21,39 +21,41 @@ $(document).ready(function () {
         modal.css("display", "block");
     })
 
-   saveBtn.on("click", function (event) {
+    // // Does a post to the createStudent route and redirects to students if successful
+    function addStudent(first_name, last_name, teacher_id) {
+        $.post("/api/createStudent", {
+            first_name: first_name,
+            last_name: last_name,
+            TeacherId: teacher_id
+        })
+            .then(function (data) {
+                // window.location.replace("/students");
+                window.location.href = "/students";
+                console.log("successfully saved!")
+            })
+    }
+
+    saveBtn.on("click", function (event) {
         event.preventDefault();
-        
+
         // console.log("clicked");
 
         let newStudent = {
             first_name: firstNameInput.val().trim(),
-			last_name: lastNameInput.val().trim(),
-			teacher_id: teacherIdInput.val().trim()
+            last_name: lastNameInput.val().trim(),
+            teacher_id: teacherIdInput.val().trim()
         }
 
         console.log(newStudent);
         if (!newStudent.first_name || !newStudent.last_name || !newStudent.teacher_id) {
-			return;
+            return;
         }
-        
+
         // Run function addStudent if fields are valid
-		addStudent(newStudent.first_name, newStudent.last_name, newStudent.teacher_id);
-		firstNameInput.val("");
-		lastNameInput.val("");
-		teacherIdInput.val("");
-		// // Does a post to the createStudent route and redirects to students if successful
-		function addStudent(first_name, last_name, teacher_id) {
-			$.post("/api/createStudent", {
-				first_name: first_name,
-				last_name: last_name,
-				TeacherId: teacher_id
-			})
-				.then(function (data) {
-                    // window.location.replace("/students");
-                    window.location.href = "/students";
-                    console.log("successfully saved!")
-				})
-		}
+        addStudent(newStudent.first_name, newStudent.last_name, newStudent.teacher_id);
+        firstNameInput.val("");
+        lastNameInput.val("");
+        teacherIdInput.val("");
+
     })
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,17 +3,17 @@ var db = require("../models");
 var passport = require("../config/passport");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/signin", passport.authenticate("local"), function(req, res) {
+  app.post("/api/signin", passport.authenticate("local"), function (req, res) {
     res.json(req.user);
   });
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/createAccount", function(req, res) {
+  app.post("/api/createAccount", function (req, res) {
     console.log("---------------------------------------posted--------------------------------------------");
     db.Teacher.create({
       first_name: req.body.first_name,
@@ -22,18 +22,18 @@ module.exports = function(app) {
       password: req.body.password,
     })
 
-    // res.redirect(307);
-      .then(function() {
+      // res.redirect(307);
+      .then(function () {
         console.log("---------------------redirecting-----------------------");
         res.redirect(307, "/api/signin");
       })
-      // .catch(function(err) {
-      //   res.status(401).json(err);
-      // });
+    // .catch(function(err) {
+    //   res.status(401).json(err);
+    // });
   });
 
   // Post request to add a student to database comes from createStudent.js
-  app.post("/api/createStudent", function(req, res) {
+  app.post("/api/createStudent", function (req, res) {
     console.log("--------------etudiant cree-----------------------");
     console.log(req.body);
     db.Student.create({
@@ -41,21 +41,23 @@ module.exports = function(app) {
       last_name: req.body.last_name,
       TeacherId: req.body.TeacherId
     })
-    .then(function(data) {
-      console.log(data);
-    })
-    res.status(200).end();
-    res.redirect(307);
+      .then(function (data) {
+        console.log(data);
+        res.status(200).end();
+        // res.redirect(307);
+      })
+   
+   
   });
 
-// Route for logging user out
-  app.get("/logout", isAuthenticated, function(req, res) {
+  // Route for logging user out
+  app.get("/logout", isAuthenticated, function (req, res) {
     req.logout();
     res.redirect("/");
   });
 
-// Route for getting some data about our user to be used client side
-  app.get("/api/user_data", isAuthenticated, function(req, res) {
+  // Route for getting some data about our user to be used client side
+  app.get("/api/user_data", isAuthenticated, function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       // isAuthenticated();
@@ -69,63 +71,69 @@ module.exports = function(app) {
       });
     }
   });
-//Student
-  app.get("/api/students", isAuthenticated, function(req, res) {
+  //Student
+  app.get("/api/students", isAuthenticated, function (req, res) {
     db.Student.findAll({})
-      .then(function(studentData) {
+      .then(function (studentData) {
         res.json(studentData);
       });
   });
 
-  app.post("/api/createStudent", function(req, res) {
-    console.log("---------------------------------------created student--------------------------------------------");
-    db.Student.create({
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      teacherId: req.body.teacherId,
-    })
-  });
+  // app.post("/api/createStudent", function(req, res) {
+  //   console.log("---------------------------------------created student--------------------------------------------");
+  //   db.Student.create({
+  //     first_name: req.body.first_name,
+  //     last_name: req.body.last_name,
+  //     teacherId: req.body.teacherId,
+  //   })
+  // });
 
-//Assignment
-  app.get("/api/assignments", isAuthenticated, function(req, res) {
+  //Assignment
+  app.get("/api/assignments", isAuthenticated, function (req, res) {
     db.Assignment.findAll({})
-      .then(function(assignmentData) {
+      .then(function (assignmentData) {
         res.json(assignmentData);
       });
-      console.log("--------------------------");
+    console.log("--------------------------");
+    // res.status(200).end();
+    // res.redirect(307);
   });
   // to get a single assignment
-  app.get("/api/assignment/:id", function(req, res) {
+  app.get("/api/assignment/:id", function (req, res) {
     // Here we add an "include" property to our options in our findOne query
     db.Assignment.findOne({
       where: {
         id: req.params.id
       },
-    }).then(function(dbAssignment) {
+    }).then(function (dbAssignment) {
       res.json(dbAssignment);
     });
   });
 
-  app.post("/api/createAssignment", function(req, res) {
+  app.post("/api/createAssignment", function (req, res) {
     console.log(req.body)
     console.log("---------------------------------------created assignment--------------------------------------------");
-    db.Assignment.create({
-      title: req.body.title,
-      description: req.body.description,
-      assignment_date: req.body.assignment_date,
-      due_date: req.body.due_date,
-      subject: req.body.subject
-    }).then (function (){res.end();})
-      // .then(function() {
-      //   console.log("---------------------redirecting-----------------------");
-      //   res.redirect(307, "");
-      // })
-      // .catch(function(err) {
-      //   res.status(401).json(err);
-      // });
+    if (req.body.title.length !== 0) {
+      db.Assignment.create({
+        title: req.body.title,
+        description: req.body.description,
+        assignment_date: req.body.assignment_date,
+        due_date: req.body.due_date,
+        subject: req.body.subject
+      }).then(function (results) {
+        res.end();
+      })
+    }
+    // .then(function() {
+    //   console.log("---------------------redirecting-----------------------");
+    //   res.redirect(307, "");
+    // })
+    // .catch(function(err) {
+    //   res.status(401).json(err);
+    // });
   });
 
-  app.put("/api/assignments/:id", function(req, res) {
+  app.put("/api/assignments/:id", function (req, res) {
     console.log(req.params.id)
     console.log(req)
     db.Assignment.update(
@@ -134,36 +142,36 @@ module.exports = function(app) {
         where: {
           id: req.params.id
         }
-      }).then(function(dbAssignment) {
-      res.json(dbAssignment);
-    });
+      }).then(function (dbAssignment) {
+        res.json(dbAssignment);
+      });
   });
 
-  app.delete("/api/assignments/:id", function(req, res) {
+  app.delete("/api/assignments/:id", function (req, res) {
     db.Assignment.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbAbacus) {
+    }).then(function (dbAbacus) {
       res.json(dbAbacus);
     });
   });
 
-//Grade
-app.get("/api/grades", isAuthenticated, function(req, res) {
-  db.Grade.findAll({})
-    .then(function(gradeData) {
-      res.json(gradeData);
-    });
+  //Grade
+  app.get("/api/grades", isAuthenticated, function (req, res) {
+    db.Grade.findAll({})
+      .then(function (gradeData) {
+        res.json(gradeData);
+      });
     console.log("--------------------------");
-});
-app.post("/api/addGrade", function(req, res) {
-  console.log("---------------------------------------added grade--------------------------------------------");
-  db.Grade.create({
-    gradeVal: req.body.gradeVal,
-    assignmentId: req.body.assignmentId,
-    studentId: req.body.studentId,
-  })
-});
+  });
+  app.post("/api/addGrade", function (req, res) {
+    console.log("---------------------------------------added grade--------------------------------------------");
+    db.Grade.create({
+      gradeVal: req.body.gradeVal,
+      assignmentId: req.body.assignmentId,
+      studentId: req.body.studentId,
+    })
+  });
 
 };
